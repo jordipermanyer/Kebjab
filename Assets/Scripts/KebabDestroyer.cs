7bc78976f3774af71be15f19c4e9d07434df923b
DanielBenitez2 <dbenitezsoler@gmail.com>
2024-01-09 22:08:40 +0100

    Taques de kebab afegides
    YA ESTA HECHOO TODO

--- Assets/Scripts/KebabDestroyer.cs
+++ Assets/Scripts/KebabDestroyer.cs
@@ -7,6 +7,8 @@ public class KebabDestroyer : MonoBehaviour
 {
     AudioClip kebabSplash;
     private AudioSource sound;
+    public GameObject Manchakebab;
+    private GameObject manchakebabOld;
     
 
     void Start()
@@ -19,17 +21,22 @@ public class KebabDestroyer : MonoBehaviour
     {
         if (col.gameObject.CompareTag("kebab"))
         {
+            manchakebabOld = Instantiate(Manchakebab, col.gameObject.transform.position, Quaternion.Euler(0f, 0f, 0f));
             Destroy(col.gameObject);
+            Destroy(manchakebabOld, 5f);
             sound.PlayOneShot(kebabSplash);
             Debug.Log("Object Destroyed");
             ScoreManager.instance.looseLife();
             
+
 
         }
         if (col.gameObject.CompareTag("kebabPartido"))
         {
             Destroy(col.gameObject);
         }
+
+        
 
     }
 }
